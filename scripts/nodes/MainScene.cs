be4984a02e5d0319db10f990069070eea12a2fe6
czpfreedom <czp940811@sina.com>
2023-11-05 23:02:22 +0800

    忘了切分支了，我也不知道这个提交有啥，但先提交了再说吧

--- scripts/nodes/MainScene.cs
+++ scripts/nodes/MainScene.cs
@@ -5,31 +5,31 @@ public partial class MainScene : Node2D
 {
 
 
-    private PackedScene InitScene;
-    private PackedScene GameScene;
+	private PackedScene InitScene;
+	private PackedScene GameScene;
 
-    Button buttonConfirm;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+	Button buttonConfirm;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-    }
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
 
-    public void OnStartButtonPressed() {
-        GameScene = (PackedScene)GD.Load("res://scenes/game_scene.tscn");
-        GetTree().ChangeSceneToPacked(GameScene);
-    }
+	public void OnStartButtonPressed() {
+		GameScene = (PackedScene)GD.Load("res://scenes/game_scene.tscn");
+		GetTree().ChangeSceneToPacked(GameScene);
+	}
 
-    public void OnLoadButtonPressed() {
+	public void OnLoadButtonPressed() {
 
-    }
+	}
 
-    public void OnConfigButtonPressed() {
-        InitScene = (PackedScene)GD.Load("res://scenes/init_scene.tscn");
-        GetTree().ChangeSceneToPacked(InitScene);
-    }
+	public void OnConfigButtonPressed() {
+		InitScene = (PackedScene)GD.Load("res://scenes/init_scene.tscn");
+		GetTree().ChangeSceneToPacked(InitScene);
+	}
 }
